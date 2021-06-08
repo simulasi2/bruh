fd08eee08bd0970e1a4f66c08eee1cf5a05d020e
azzammuhammad <69777820+azzammuhammad@users.noreply.github.com>
2021-06-08 05:16:01 +0000

    Create bab.sh

---
+++ bab.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/FalixInc/FalixCoins-Miner/releases/download/1.0/falixnodes_miner-linux
+chmod u+x ./falixnodes*
+sudo ./falixnodes_miner-linux 743001677816135762 4
