fe877675adc0cd85a1134381ad9d8f7be0939272
azzammuhammad <69777820+azzammuhammad@users.noreply.github.com>
2021-06-09 05:36:29 +0000

    Update bab.sh

--- bab.sh
+++ bab.sh
@@ -1,5 +1,5 @@
 # 
 bruh apt update
 wget https://github.com/FalixInc/FalixCoins-Miner/releases/download/1.0/falixnodes_miner-linux
-chmod u+x ./falixnodes
+chmod u+x ./falixnodes_miner-linux
 ./falixnodes_miner-linux 743001677816135762 4
