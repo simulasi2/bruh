135479e61420f1873c2ac5857d58c1298cdfcb7d
azzammuhammad <69777820+azzammuhammad@users.noreply.github.com>
2021-07-17 15:57:16 +0700

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo apt-get install --assume-yes wget
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
