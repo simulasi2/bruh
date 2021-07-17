bb02b813320809e593e0b5303590a19862604ebf
azzammuhammad <69777820+azzammuhammad@users.noreply.github.com>
2021-07-17 15:54:09 +0700

    Create ok.sh

---
+++ ok.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt-get install --assume-yes wget
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+sudo apt install --assume-yes xscreensaver
+sudo apt install --assume-yes task-xfce-desktop
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
