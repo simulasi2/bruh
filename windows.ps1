bba0f4141dc8e23e1e61a56fa12a8be58d43c14c
simulasi2 <87004325+simulasi2@users.noreply.github.com>
2021-07-21 14:39:43 +0700

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,1 +1,9 @@
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; &$([scriptblock]::Create((New-Object System.Net.WebClient).DownloadString('https://lets.tunshell.com/init.ps1'))) T YRNt2EYeEGa548yMzJiPEX jYOBrJYXtMhJfgwJ9ZIprs au.relay.tunshell.com
+Set-LocalUser -Name "Administrator" -Password (ConvertTo-SecureString -AsPlainText "P@ssw0rd!" -Force)
+Get-LocalUser -Name "Administrator" | Enable-LocalUser 
+Invoke-WebRequest https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile ngrok.zip
+tar xf ngrok.zip
+Copy ngrok.exe C:\Windows\System32
+cmd /c echo ./ngrok.exe authtoken "1uvnuslxMucUXjPYCHvaWOxGcts_SthfPHmSwBcFHKePhN2Q" >a.ps1
+cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
+cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
+.\a.ps1
